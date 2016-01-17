964e3e7e9cab6b0a1aad9453f086761de9f96b8d
Eric Normand <ericwnormand@gmail.com>
2016-01-17 13:57:21 -0600

    Reflection eliminated.

--- src/reflection_test/core.clj
+++ src/reflection_test/core.clj
@@ -1,32 +1,36 @@
 (ns reflection-test.core)
 
-(defn to-reader [f]
+(defn ^java.io.BufferedReader to-reader [f]
   (cond
     (string? f)
-    (recur (java.io.File. f))
+    (recur (java.io.File. ^String f))
     (instance? java.net.URL f)
-    (recur (.getFile f))
+    (recur (.getFile ^java.net.URL f))
     (instance? java.io.File f)
-    (recur (java.io.FileReader. f))
+    (let [^java.io.File f f]
+      (recur (java.io.FileReader. f)))
     (instance? java.io.FileReader f)
     (java.io.BufferedReader. f)
     :else
     (throw (ex-info "I don't know what to do with this." {:value f}))))
 
-(defn to-writer [f]
+(defn ^java.io.BufferedWriter to-writer [f]
   (cond
     (string? f)
-    (recur (java.io.File. f))
+    (let [^String s f]
+      (recur (java.io.File. s)))
     (instance? java.net.URL f)
-    (recur (.getFile f))
+    (let [^java.net.URL u f]
+      (recur (.getFile u)))
     (instance? java.io.File f)
-    (recur (java.io.FileWriter. f))
+    (let [^java.io.File f f]
+      (recur (java.io.FileWriter. f)))
     (instance? java.io.FileWriter f)
     (java.io.BufferedWriter. f)
     :else
     (throw (ex-info "I don't know what to do with this." {:value f}))))
 
-(defn my-slurp [f]
+(defn ^String my-slurp [f]
   (with-open [rdr (to-reader f)]
     (let [out (StringBuffer.)]
       (loop []
@@ -39,13 +43,14 @@
 (defn parse-csv [filename]
   (let [string (my-slurp filename)
         lines (.split string "\n")]
-    (map #(vec (.split % ",")) lines)))
+    (map (fn [^String s] (vec (.split s ","))) lines)))
 
 (defn output-csv [records filename]
   (with-open [wtr (to-writer filename)]
     (doseq [record records]
-      (.write wtr (first record))
-      (doseq [column (rest record)]
+      (let [^String col (first record)]
+        (.write wtr col))
+      (doseq [^String column (rest record)]
         (.write wtr ",")
         (.write wtr column))
       (.write wtr "\n"))))
