fa727b87088fe369fb4e25bdc91f4b91aa2f05c5
Eric Normand <ericwnormand@gmail.com>
2016-01-16 18:16:43 -0600

    No warn on reflection.

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :global-vars {*warn-on-reflection* true})
+  :dependencies [[org.clojure/clojure "1.7.0"]])
